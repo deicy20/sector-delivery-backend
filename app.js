58a484171021b6e3393d6416d190121a4f12b973
Deicy León <54363695+deicy20@users.noreply.github.com>
2025-04-15 12:38:28 -0500

    correccion

--- app.js
+++ app.js
@@ -2,30 +2,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const sectorRoutes = require("./src/routes/sectorRoutes");
-
-const admin = require("firebase-admin");
+const { db } = require("./src/config/firebase"); // Aquí importas el db ya inicializado
 
 const app = express();
-
-const firebaseCredentials = {
-  type: "service_account",
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN || "googleapis.com",
-};
-
-admin.initializeApp({
-  credential: admin.credential.cert(firebaseCredentials),
-});
-
-const db = admin.firestore();
 
 app.use(cors());
 app.use(express.json());
