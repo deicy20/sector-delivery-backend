70e8e1ab5f675d3646871f167022d57928b1c006
Deicy León <54363695+deicy20@users.noreply.github.com>
2025-04-15 11:00:16 -0500

    Add dynamic Firebase credential generation

--- app.js
+++ app.js
@@ -4,6 +4,26 @@ const cors = require("cors");
 const sectorRoutes = require("./src/routes/sectorRoutes");
 
 const app = express();
+const fs = require("fs");
+
+const firebaseCredentials = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN || "googleapis.com",
+};
+
+fs.writeFileSync(
+  "./src/firebase-credentials.json",
+  JSON.stringify(firebaseCredentials)
+);
 
 app.use(cors());
 app.use(express.json());
